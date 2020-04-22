29ceb61e7bf68e22a1170cf4c5eabd41d99ab907
Jeong <inyeop0212@gmail.com>
2020-04-23 02:37:31 +0900

    Create Song schema

---
+++ models/song.js
@@ -0,0 +1,42 @@
+import mongoose from "mongoose";
+
+const schema = mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  artist_name: {
+    type: String,
+    required: true
+  },
+  artist_thumbnail: {
+    type: String,
+    default: ''
+  },
+  video_url: {
+    type: String,
+    default: ''
+  },
+  music_url: {
+    type: String,
+    required: true
+  },
+  music_thumbnail: {
+    type: String,
+    default: ''
+  },
+  note: [
+    {
+      time: { type: Number, required: true },
+      key: {type: String, required: true }
+    }
+  ],
+  ranking: [
+    {
+      username: { type: String, required: true },
+      score: { type: Number, required: true }
+    }
+  ]
+});
+
+export default mongoose.model("Song", schema);
