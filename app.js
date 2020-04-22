59a4497be83568f9768c3438a7d0bee08da0dade
Jeong <inyeop0212@gmail.com>
2020-04-23 02:02:15 +0900

    Handle 404 Error & Make Custom Error class

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import path from "path";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
+import { CustomError, errorHandler } from "./lib/error";
 
 const app = express();
 
@@ -13,5 +14,13 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use("/", indexRouter);
+
+app.use(function (req, res, next) {
+  next(new CustomError(404, "Not Found"));
+});
+
+app.use(function (err, req, res, next) {
+  errorHandler(err, res);
+});
 
 module.exports = app;
