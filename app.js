19a1069e0836d03aa2c9c43c1fc7f5ec67de1418
Jeong <inyeop0212@gmail.com>
2020-04-25 00:39:34 +0900

    Use cors

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import path from "path";
+import cors from "cors";
 import cookieParser from "cookie-parser";
 import logger from "morgan";
 import indexRouter from "./routes/index";
@@ -9,6 +10,7 @@ import "./db";
 
 const app = express();
 
+app.use(cors());
 app.use(logger("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
